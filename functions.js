cc49b5cf1105ae6cb6fd37b55acd9e22a5b4e69f
MariL11 <RMariL11777@gmail.com>
2024-09-12 00:41:01 +0200

    Modificaciones

--- functions.js
+++ functions.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const pauseIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#000000" d="M48 64C21.5 64 0 85.5 0 112L0 400c0 26.5 21.5 48 48 48l32 0c26.5 0 48-21.5 48-48l0-288c0-26.5-21.5-48-48-48L48 64zm192 0c-26.5 0-48 21.5-48 48l0 288c0 26.5 21.5 48 48 48l32 0c26.5 0 48-21.5 48-48l0-288c0-26.5-21.5-48-48-48l-32 0z"/></svg>`;
 
     //Animación del volumen
-    const canvas = document.getElementById('volumen');
+    const canvas = document.querySelector('.volumen');
     const bars = canvas.getContext('2d');
     const numberBar = 80;
     const barWidth = 4;
